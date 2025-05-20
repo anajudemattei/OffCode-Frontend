fc92c945facb08b764b6ebe6bf0b5a0ae8250b4b
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2025-05-20 11:58:01 -0300

    Feat: updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import Navigation from "@/components/Navigation";
 
 const font = Roboto ({
   variable: "--font",
@@ -19,7 +21,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html>
-            <body className={font.variable}>{children}</body>
+            <body className={font.variable}>
+              <Header />
+              <Navigation />
+              {children}
+              </body>
         </html>
     );
 }
