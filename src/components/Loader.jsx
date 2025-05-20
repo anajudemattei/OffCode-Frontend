79965e25de5bbfaec01d51fbd00366e1a2ac2dd7
AnaCarolinaFreitas <anacarolinagarciafreitas@gmail.com>
2025-05-20 16:19:14 -0300

    Fix: image Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/Loader.module.css";
 export default function Loader() {
     return (
         <div className={styles.container}>
-            <Image src="/loading.gif" alt="Loading..." width={300} height={300} priority unoptimized className={styles.image} />
+            <Image src="/images/loading.gif" alt="Loading..." width={300} height={300} priority unoptimized className={styles.image} />
             <h1 className={styles.message}>Loading ...</h1>
         </div>
     );
