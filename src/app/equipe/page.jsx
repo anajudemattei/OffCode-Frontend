28aa8e37cabfa8519bdf90aabaabadc1e46c5738
anajudemattei <anajuliademattei26@gmail.com>
2025-05-28 22:17:22 -0300

    feat: continuação dos ajustes na pagina da equipe

--- src/app/equipe/page.jsx
+++ src/app/equipe/page.jsx
@@ -2,13 +2,13 @@
 
 import styles from "./equipe.module.css";
 import Navigation from "@/components/Navigation";
-import Noticias from "@/components/Noticias";
+import Header from "@/components/Header";
 import Image from "next/image";
 import FlipCard from "@/components/FlipCard";
 
 export default function Equipe() {
     const equipe = [
-        { nome: "Ana Carolina", cargo: "Scrum Master", imagem: "/images/luanaCard.png" },
+        { imagem: "/images/luanaCard.png" },
         { nome: "Ana Julia", cargo: "Desenvolvedora", imagem: "/images/luanaCard.png" },
         { nome: "Bernardo", cargo: "Desenvolvedor", imagem: "/images/luanaCard.png" },
         { nome: "Beatriz", cargo: "Desenvolvedora", imagem: "/images/luanaCard.png" },
@@ -17,28 +17,30 @@ export default function Equipe() {
     ];
 
     return (
-        <div className={styles.pageContainer}>
-            <Navigation />
-            <div className={styles.equipeContainer}>
-                <div className={styles.headerSection}>
-                    <h1>Conheça Quem Está por Trás do Código</h1>
-                    <p>
-                        Nosso time de desenvolvedores une talento e inovação para transformar ideias em soluções digitais eficientes.
-                    </p>
-                </div>
-                <div className={styles.cardsContainer}>
-                    {equipe.map((membro, index) => (
-                        <FlipCard
-                            key={index}
-                            frontImage={membro.imagem}
-                            backTitle={membro.nome}
-                            backText1={membro.cargo}
-                            backText2="Parte da equipe OffCode"
-                        />
-                    ))}
-                </div>
-            </div>
-            <Noticias />
-        </div>
+       <>
+  <Header />
+  <div className={styles.pageContainer}>
+    <Navigation />
+    <div className={styles.mainContent}>
+      <div className={styles.headerSection}>
+        <h1>Conheça Quem Está por Trás do Código</h1>
+        <p>
+          Nosso time de desenvolvedores une talento e inovação para transformar ideias em soluções digitais eficientes.
+        </p>
+      </div>
+      <div className={styles.cardsContainer}>
+        {equipe.map((membro, index) => (
+          <FlipCard
+            key={index}
+            frontImage={membro.imagem}
+            backTitle={membro.nome}
+            backText1={membro.cargo}
+            backText2="Parte da equipe OffCode"
+          />
+        ))}
+      </div>
+    </div>
+  </div>
+</>
     );
 }
